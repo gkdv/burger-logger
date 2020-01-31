3579c93ddcabdfe931665421a6870b869d4e22f9
garrett krage <garrett.krage@gmail.com>
2020-01-30 21:17:33 -0700

    fixed some bugs.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ('cheese burger');
-INSERT INTO burgers (name) VALUES ('chili burger');
-INSERT INTO burgers (name, sleepy) VALUES ('vegan burger', true);
-INSERT INTO burgers (name, sleepy) VALUES ('meat sauce burger', true);
-INSERT INTO burgers (name, sleepy) VALUES ('donald burger', true);
-INSERT INTO burgers (name) VALUES ('hillary burger');
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('chili burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('vegan burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('meat sauce burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('donald burger', true);
+INSERT INTO burgers (burger_name) VALUES ('hillary burger');
