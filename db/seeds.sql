fa25f1fafd0f3bb96c89b0d868ab545cb790da32
garrett krage <garrett.krage@gmail.com>
2020-01-30 20:19:06 -0700

    added .sql functionality

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('cheese burger');
+INSERT INTO burgers (name) VALUES ('chili burger');
+INSERT INTO burgers (name, sleepy) VALUES ('vegan burger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('meat sauce burger', true);
+INSERT INTO burgers (name, sleepy) VALUES ('donald burger', true);
+INSERT INTO burgers (name) VALUES ('hillary burger');
