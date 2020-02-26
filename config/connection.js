7689802882a67573ac1918598fcdea4fab8460ce
garrett krage <garrett.krage@gmail.com>
2020-02-26 15:11:47 -0700

    fixed jawsDB connection for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 var mysql = require("mysql");
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DB
-});
+} else {
+  var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DB
+  });
+
+}
+
 
 // Make connection.
 connection.connect(function(err) {
